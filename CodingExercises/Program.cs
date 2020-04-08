bd930dea72c7bcb1345cfd6004351ba1ce05f2c1
Ori Lindner <olindner@umich.edu>
2020-04-08 13:37:32 -0400

    First

---
+++ CodingExercises/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace CodingExercises
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
